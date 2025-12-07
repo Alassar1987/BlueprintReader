038b57ecc09f56dc4a8af338e200afa58d50fbe0
Alassar1987 <sergeysvichkar@gmail.com>
2025-12-07 17:40:38 +1000

    Добавил комментариев в хедер Экстрактора Компонента

--- Source/BlueprintReader/Public/Extractors/BPR_Extractor_ActorComponent.h
+++ Source/BlueprintReader/Public/Extractors/BPR_Extractor_ActorComponent.h
@@ -17,49 +17,76 @@ class UEdGraphPin;
 class BLUEPRINTREADER_API BPR_Extractor_ActorComponent
 {
 public:
-	BPR_Extractor_ActorComponent();
-	~BPR_Extractor_ActorComponent();
-
-	/** Главная точка входа: обработка выбранного ассета */
+	// -------------------------------
+	// Главная точка входа
+	// -------------------------------
+	/** Обрабатывает выбранный объект (Blueprint), формирует структуру и граф */
 	void ProcessComponent(UObject* SelectedObject, FBPR_ExtractedData& OutData);
 
 private:
+
+	// -------------------------------
 	// Логирование
+	// -------------------------------
+	// Простые методы для вывода сообщений разного уровня
 	void LogMessage(const FString& Msg);
 	void LogWarning(const FString& Msg);
 	void LogError(const FString& Msg);
 
-	// Основная обработка Blueprint
+	// -------------------------------
+	// Работа со структурой Blueprint
+	// -------------------------------
+	/** Общая информация о Blueprint (родительский класс, репликация) */
 	void AppendBlueprintInfo(UBlueprint* Blueprint, FString& OutText);
-	void AppendVariables(UBlueprint* Blueprint, FString& OutText);
-	FString GetPropertyDefaultValue(FProperty* Property, UObject* Object);
-	FString GetPropertyTypeDetailed(FProperty* Property);
-	void AppendStructFields(FStructProperty* StructProp, FString& OutText, int32 Indent = 0);
-	void AppendReplicationInfo(UClass* Class, FString& OutText);
-	void AppendGraphs(UBlueprint* Blueprint, FString& OutText);
-	
-	void AppendGraphSequence(
-	UEdGraph* Graph, 
-	FString& OutExecText,
-	FString& OutDataText
-	);
-	
-	void ProcessNodeSequence(
-	UEdGraphNode* Node, 
-	int32 IndentLevel, 
-	TSet<UEdGraphNode*>& Visited, 
-	FString& OutExecText,
-	FString& OutDataText
-	);
 
+	/** Сбор пользовательских переменных, включая тип, дефолтное значение, флаги, категорию, описание */
+	void AppendVariables(UBlueprint* Blueprint, FString& OutText);
+
+	/** Возвращает дефолтное значение свойства */
+	FString GetPropertyDefaultValue(FProperty* Property, UObject* Object);
+
+	/** Возвращает детализированный тип свойства, включая TMap/TSet/TArray и enum */
+	FString GetPropertyTypeDetailed(FProperty* Property);
+
+	/** Обрабатывает поля структурных свойств и выводит их информацию */
+	void AppendStructFields(FStructProperty* StructProp, FString& OutText, int32 Indent = 0);
+
+	/** Информация о реплицируемых переменных */
+	void AppendReplicationInfo(UClass* Class, FString& OutText);
+
+	// -------------------------------
+	// Работа с графами (Event/Function/Macro)
+	// -------------------------------
+	/** Основная точка обхода всех графов Blueprint */
+	void AppendGraphs(UBlueprint* Blueprint, FString& OutText);
+
+	/** Обход последовательности узлов графа (exec + data flow) */
+	void AppendGraphSequence(UEdGraph* Graph, FString& OutExecText, FString& OutDataText);
+
+	/** Рекурсивная обработка узлов графа для exec- и data-flow */
+	void ProcessNodeSequence(UEdGraphNode* Node, int32 IndentLevel, TSet<UEdGraphNode*>& Visited, FString& OutExecText, FString& OutDataText);
+
+	/** Сбор подписи функции по входным/выходным пинам */
 	FString GetFunctionSignature(UEdGraph* Graph);
+
+	/** Сбор подписи макроса по Tunnel-узлам */
 	FString GetMacroSignature(UEdGraph* Graph);
+
+	/** Формирует читаемое имя узла, учитывая тип узла и параметры */
 	FString GetReadableNodeName(UEdGraphNode* Node);
+
+	/** Детали пина (значение по умолчанию или ссылка на другой узел) */
 	FString GetPinDetails(UEdGraphPin* Pin);
-	
+
+	/** Читабельное имя пина для вывода */
 	FString GetPinDisplayName(UEdGraphPin* Pin);
+
+	// -------------------------------
+	// Вспомогательные методы
+	// -------------------------------
+	/** Очищает имя от хвоста GUID, если он есть */
 	FString CleanName(const FString& RawName);
 
-	
+	/** Определяет, является ли свойство пользовательской переменной (не системной) */
 	bool IsUserVariable(FProperty* Property);
 };
